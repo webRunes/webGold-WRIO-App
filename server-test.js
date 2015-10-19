92c13a481eaa2bca0d44fc1af11080ec6600f61b
Alexey Anshakov <AlexeyAnshakov@users.noreply.github.com>
2015-10-19 16:05:03 +0200

    Revert "Unit tests imroved"

---
+++ server-test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by michbil on 08.10.15.
+ */
+/*try {
+    require("babel/register")({
+        stage: 0
+    });
+} catch (e) {
+    console.log("Babel polyfill already loaded, seems to be ok, relaxing....");
+}*/
+
+import app from './src'
+
+module.exports = app;
