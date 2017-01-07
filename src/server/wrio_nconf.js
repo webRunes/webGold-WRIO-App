c8eed1db4619d8df411db910a173c7fca6ee78d3
Michael Bilenko <denso.ffff@gmail.com>
2017-01-08 00:45:35 +0200

    pre

--- src/server/wrio_nconf.js
+++ src/server/wrio_nconf.js
@@ -1,15 +1,16 @@
 import path from 'path';
 import nconf from 'nconf';
-import logger from 'winston';
 
 nconf.env().argv();
 
 var basedirPath = path.dirname(require.main.filename); // won't work with unit tests
+
 if (process.env.WRIO_CONFIG) {
     nconf.file(path.resolve(__dirname, `../../${process.env.WRIO_CONFIG}`));
-    logger.info(`${process.env.WRIO_CONFIG} config loaded`);
 } else {
     nconf.file(path.resolve(__dirname, '../../config.json'));
 }
+
+console.log("Sample configuration loaded");
 
 export default nconf;
