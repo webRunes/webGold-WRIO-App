a8e8f517d027c856193fef5a9702631957e77462
Michael Bilenko <denso.ffff@gmail.com>
2017-01-20 20:25:31 +0200

    add binaries

--- contract/deploy-contract.js
+++ contract/deploy-contract.js
@@ -9,7 +9,11 @@ import {db as dbMod} from '../src/server/common';var init = dbMod.init;
 import {dumpError} from '../src/server/common/utils/utils.js';
 import solc from 'solc';
 
+const COMPILER_VER = "v0.4.8+commit.60cc1668";
+
 class WebGoldDeploy extends WebGold {
+
+
 
 
     async compileDeploy(contractName) {
@@ -18,15 +22,22 @@ class WebGoldDeploy extends WebGold {
             const web3 = this.getWeb3();
             console.log("Compiling...");
             const source = fs.readFileSync(`./src/${contractName}.sol`).toString();
-            let compiledContract = solc.compile(source, 1);
-            let abi = compiledContract.contracts[contractName].interface;
-            let bytecode = compiledContract.contracts[contractName].bytecode;
-            fs.writeFileSync(`./bin/${contractName}.abi`,abi);
-            fs.writeFileSync(`./bin/${contractName}.binary`,bytecode);
-            console.log("Deploying...");
-            this.unlockMaster();
-            const contraddr = await this.deploy("0x740f63f535bc86fb87f9482adbec5ca289a2d59e", bytecode,JSON.parse(abi));
-            this.saveContractAddress(contraddr,`./bin/${contractName}.addr`);
+            solc.loadRemoteVersion(COMPILER_VER, async (err, solcSnapshot) => {
+                console.log(`Compiler version ${COMPILER_VER} downloaded`);
+                if (err) {
+                    return err;
+                }
+                let compiledContract = solcSnapshot.compile(source, 1);
+                let abi = compiledContract.contracts[contractName].interface;
+                let bytecode = compiledContract.contracts[contractName].bytecode;
+                fs.writeFileSync(`./bin/${contractName}.abi`,abi);
+                fs.writeFileSync(`./bin/${contractName}.binary`,bytecode);
+                console.log("Deploying...");
+                this.unlockMaster();
+                const contraddr = await this.deploy("0x740f63f535bc86fb87f9482adbec5ca289a2d59e", bytecode,JSON.parse(abi));
+                this.saveContractAddress(contraddr,`./bin/${contractName}.addr`);
+            });
+
 
         } catch(e) {
             console.log(e);
